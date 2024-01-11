c79852d20e346d6c99749b05fcfff7f5ce933719
badrbnh <badrechahide@gmail.com>
2024-01-11 21:32:16 +0100

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,5 +7,5 @@ from math import sqrt
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''function to_kv that takes a string k and
     an int OR float v as arguments and returns a tuple. '''
-    square: float = sqrt(v)
+    square: float = v ** 2
     return (k, square)
