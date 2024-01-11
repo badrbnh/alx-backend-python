f9852714f3151bc3f25b0b27378282b25b5e170c
badrbnh <badrechahide@gmail.com>
2024-01-11 19:35:16 +0100

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+'''Concatonation function'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Takes two string arguments and returns a concatonated string'''
+    return str1 + str2
