fa646b66fa739ab59b88b114db2614d1b7c7982a
badrbnh <badrechahide@gmail.com>
2024-02-05 19:14:16 +0100

    fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Tests for utilss"""
 import unittest
-from nose.tools import assert_equal
 from parameterized import parameterized, parameterized_class
 import utils
 
@@ -9,10 +8,10 @@ import utils
 class TestAccessNestedMap(unittest.TestCase):
     """Class to test Access nested map"""
     @parameterized.expand([
-        ({"a": 1}, ("a",),1),
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """test access nested map"""
-        assert_equal(utils.access_nested_map(nested_map, path), expected)
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
